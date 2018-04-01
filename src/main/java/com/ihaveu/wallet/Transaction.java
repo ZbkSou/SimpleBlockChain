1f2556d8344797fe82876e92e51ad536d2905c97
Your Name <zhoubk1451@163.com>
2018-04-01 22:28:31 +0800

    rm

--- src/main/java/com/ihaveu/wallet/Transaction.java
+++ src/main/java/com/ihaveu/wallet/Transaction.java
@@ -16,6 +16,7 @@ public class Transaction {
     public float value;
     public byte[] signature;
 
+    
     public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
     public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
     private static int sequence = 0; // a rough count of how many transactions have been generated.
