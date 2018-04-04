94405a777c56c1b96ba074ec7f2a30041c8602a9
baokangzhou <zhoubk1451@163.com>
2018-04-04 19:52:03 +0800

    hggjhgjhjhjhhjhjk

--- src/main/java/com/ihaveu/wallet/TransactionInput.java
+++ src/main/java/com/ihaveu/wallet/TransactionInput.java
@@ -4,4 +4,10 @@ package com.ihaveu.wallet;
  * Created by ZBK on 2018-04-01.
  */
 public class TransactionInput {
+    public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; //Contains the Unspent transaction output
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
 }
